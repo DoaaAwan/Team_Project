ca638706c1f3e0e3d7117c0c4800166594795cf3
ChristianSmallw <christian.smallw@gmail.com>
2023-10-21 09:05:56 -0400

    Update import-nav.js

--- scripts/import-nav.js
+++ scripts/import-nav.js
@@ -1,5 +1,9 @@
-fetch('../shared/shared-nav.html')
-.then(response => response.text())
-.then(data => {
-    document.getElementById('shared-nav').innerHTML = data;
-});
+async function import_nav(){
+    await fetch('../shared/shared-nav.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('shared-nav').innerHTML = data;
+    });
+}
+
+import_nav();
