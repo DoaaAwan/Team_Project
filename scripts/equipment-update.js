d63b81d86a4cf8ca816620d5450c9a8f924e3c6a
ChristianSmallw <christian.smallw@gmail.com>
2023-10-22 12:54:10 -0400

    Cleaned up code for equipment.
    - Equipment scripts now import equipment data from equipment.json.
    - Imports functions from functions.js.
    - All equipment code in their own script files.
    - Moved all unused script files in a 'unused scripts' folder.
    - import-nav.js loaded in head of all html pages.

--- scripts/equipment-update.js
+++ scripts/equipment-update.js
@@ -1,54 +1,23 @@
+import { fetchData } from './functions.js';
 
-const urlParams = new URLSearchParams(window.location.search);
-const equipmentID = urlParams.get('id');
+const equipmentDatabase = await fetchData('../scripts/json/equipment.json');
+const urlParam = new URLSearchParams(window.location.search);
+const equipmentId = urlParam.getAll("id");
 
-const equipmentDatabase = {
-    1: {
-        equipmentId: 1,
-        price: "$1000.00",
-        itemNumber: 332,
-        equipmentName: "GrassGlider Pro",
-        equipmentType: "Lawn Mower",
-        serialNumber: "20931890",
-        modelNumber: "A02B",
-        manufacturer: "Pro-Power Canada",
-        colour: "Red"
-    },
-    2: {
-        equipmentId: 2,
-        price: "$800.00",
-        itemNumber: 456,
-        equipmentName: "LawnMaster 2000",
-        equipmentType: "Lawn Mower",
-        serialNumber: "987654321",
-        modelNumber: "B05C",
-        manufacturer: "Troy-Bilt",
-        colour: "Green",
-    },
-};
+if (equipmentId.length > 0) {
+    document.getElementById("back-to-equipment").href = `../pages/equipment-details.html?id=${equipmentId}`
+    document.getElementById("cancel-to-details").href = `../pages/equipment-details.html?id=${equipmentId}`
+    let equipment = equipmentDatabase.find(e => e.id == equipmentId);
 
-function populateEquipmentUpdate(equipmentID) {
-
-    const equipmentUpdate = document.getElementById("equipment-update");
-
-    const equipment = equipmentDatabase[equipmentID];
-    if(equipmentID != null){
-        document.getElementById("equipment-name").value = equipment.equipmentName
-        document.getElementById("equipment-type").value = equipment.equipmentType
-        document.getElementById("manufacturer").value = equipment.manufacturer
-        document.getElementById("colour").value = equipment.colour
-        document.getElementById("item-number").value = equipment.itemNumber
-        document.getElementById("serial-number").value = equipment.serialNumber
-        document.getElementById("model-number").value = equipment.modelNumber
-    }
-    else{
-        equipmentUpdate.innerHTML = `
-        <p>No equipment was selected for update.</p>
-        <div style="margin-top: 50px;">
-            <a href="../pages/equipment-search.html" class="button btn-cancel">
-                Cancel
-            </a>
-        </div>`;
-    }
+    document.getElementById("equipment-name").value = equipment.equipmentName;
+    document.getElementById("manufacturer").value = equipment.manufacturer;
+    document.getElementById("equipment-type").value = equipment.equipmentType;
+    document.getElementById("colour").value = equipment.colour;
+    document.getElementById("item-number").value = equipment.itemNumber;
+    document.getElementById("serial-number").value = equipment.serialNumber;
+    document.getElementById("model-number").value = equipment.modelNumber;
 }
-populateEquipmentUpdate(equipmentID);
+else{
+    window.alert("No equipment selected for update.");
+    window.location.href = "../pages/equipment-search.html";
+}
