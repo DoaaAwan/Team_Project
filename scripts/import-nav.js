08d2e05263561f6719db0c8d91e6d6cf9d2cadb3
ChristianSmallw <christian.smallw@gmail.com>
2023-10-21 22:38:09 -0400

    Using jquery to load nav.

--- scripts/import-nav.js
+++ scripts/import-nav.js
@@ -1,9 +1,1 @@
-async function import_nav(){
-    await fetch('../shared/shared-nav.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('shared-nav').innerHTML = data;
-    });
-}
-
-import_nav();
+$("#shared-nav").load("../shared/shared-nav.html");
