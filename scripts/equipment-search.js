0ac5b1032996d5d9a0fc33239f928ef3a7f0972a
ChristianSmallw <christian.smallw@gmail.com>
2023-11-05 14:55:10 -0500

    Fixed seed data

--- scripts/equipment-search.js
+++ scripts/equipment-search.js
@@ -163,9 +163,6 @@ class EquipmentSearchPage {
 
     return equipmentDiv;
   }
-
-
-  
 }
 
 new EquipmentSearchPage();
@@ -268,7 +265,6 @@ document.getElementById("search-btn").addEventListener("click", function(e){
         }
     }
 });*/
-
 
 
 function getCustomerData(){
@@ -523,127 +519,115 @@ function getOwnershipData(){
 }
 
 
-function getOwnershipData(){
+function getEquipmentData(){
   return [
       {
         "id": 1,
-        "customerId": 1,
-        "equipmentId": 1,
-        "serialNumber": "6S985C78866"
+        "equipmentName": "GrassGlider Pro",
+        "equipmentType": "Lawn Mower",
+        "serialNumber": "20931890",
+        "modelNumber": "A02B",
+        "manufacturer": "Pro-Power Canada",
+        "colour": "Red"
       },
       {
         "id": 2,
-        "customerId": 1,
-        "equipmentId": 3,
-        "serialNumber": "7U942Z45486"
+        "equipmentName": "LawnMaster 2000",
+        "equipmentType": "Lawn Mower",
+        "serialNumber": "987654321",
+        "modelNumber": "B05C",
+        "manufacturer": "Troy-Bilt",
+        "colour": "Green"
       },
       {
         "id": 3,
-        "customerId": 2,
-        "equipmentId": 2,
-        "serialNumber": "2Z867N46698"
+        "equipmentName": "PowerHedge 300",
+        "equipmentType": "Hedge Trimmer",
+        "serialNumber": "123456789",
+        "modelNumber": "H12D",
+        "manufacturer": "HedgeMaster",
+        "colour": "Blue"
       },
       {
         "id": 4,
-        "customerId": 2,
-        "equipmentId": 4,
-        "serialNumber": "6D247A85967"
+        "equipmentName": "LeafBlaster 5000",
+        "equipmentType": "Leaf Blower",
+        "serialNumber": "456789123",
+        "modelNumber": "L30X",
+        "manufacturer": "Toro",
+        "colour": "Yellow"
       },
       {
         "id": 5,
-        "customerId": 3,
-        "equipmentId": 5,
-        "serialNumber": "8A979P36869"
+        "equipmentName": "SnowBuster Deluxe",
+        "equipmentType": "Snow Blower",
+        "serialNumber": "987654987",
+        "modelNumber": "S08M",
+        "manufacturer": "SnowMaster",
+        "colour": "White"
       },
       {
         "id": 6,
-        "customerId": 3,
-        "equipmentId": 7,
-        "serialNumber": "8E445U62644"
+        "equipmentName": "PressureWasher Pro",
+        "equipmentType": "Pressure Washer",
+        "serialNumber": "654321789",
+        "modelNumber": "P15T",
+        "manufacturer": "Pro-Power Canada",
+        "colour": "Silver"
       },
       {
         "id": 7,
-        "customerId": 4,
-        "equipmentId": 6,
-        "serialNumber": "5Z935M24736"
+        "equipmentName": "ChainSaw Master",
+        "equipmentType": "Chainsaw",
+        "serialNumber": "789123456",
+        "modelNumber": "C10R",
+        "manufacturer": "Craftsman",
+        "colour": "Black"
       },
       {
         "id": 8,
-        "customerId": 4,
-        "equipmentId": 8,
-        "serialNumber": "7E537T79592"
+        "equipmentName": "TurfMaster 1500",
+        "equipmentType": "Lawn Mower",
+        "serialNumber": "876543210",
+        "modelNumber": "TM15Z",
+        "manufacturer": "Pro-Power Canada",
+        "colour": "Red"
       },
       {
         "id": 9,
-        "customerId": 5,
-        "equipmentId": 9,
-        "serialNumber": "8S828W84428"
+        "equipmentName": "LeafSweeper Pro",
+        "equipmentType": "Leaf Blower",
+        "serialNumber": "123789456",
+        "modelNumber": "LS20Y",
+        "manufacturer": "Toro",
+        "colour": "Yellow"
       },
       {
         "id": 10,
-        "customerId": 5,
-        "equipmentId": 11,
-        "serialNumber": "6W727K88322"
+        "equipmentName": "EdgeTrimmer X",
+        "equipmentType": "Hedge Trimmer",
+        "serialNumber": "789456123",
+        "modelNumber": "ET10W",
+        "manufacturer": "HedgeMaster",
+        "colour": "Blue"
       },
       {
         "id": 11,
-        "customerId": 6,
-        "equipmentId": 10,
-        "serialNumber": "6V897N22689"
+        "equipmentName": "ProSnow 3000",
+        "equipmentType": "Snow Blower",
+        "serialNumber": "321654987",
+        "modelNumber": "PS30Q",
+        "manufacturer": "SnowMaster",
+        "colour": "White"
       },
       {
         "id": 12,
-        "customerId": 6,
-        "equipmentId": 12,
-        "serialNumber": "5J638F23992"
-      },
-      {
-        "id": 13,
-        "customerId": 7,
-        "equipmentId": 1,
-        "serialNumber": "9B475T53372"
-      },
-      {
-        "id": 14,
-        "customerId": 7,
-        "equipmentId": 3,
-        "serialNumber": "6H483T94664"
-      },
-      {
-        "id": 15,
-        "customerId": 8,
-        "equipmentId": 5,
-        "serialNumber": "9Y864M68339"
-      },
-      {
-        "id": 16,
-        "customerId": 8,
-        "equipmentId": 7,
-        "serialNumber": "5L383T77572"
-      },
-      {
-        "id": 17,
-        "customerId": 9,
-        "equipmentId": 9,
-        "serialNumber": "7N992J69283"
-      },
-      {
-        "id": 18,
-        "customerId": 9,
-        "equipmentId": 11,
-        "serialNumber": "7H826B47548"
-      },
-      {
-        "id": 19,
-        "customerId": 1,
-        "equipmentId": 10,
-        "serialNumber": "7Y686J58959"
-      },
-      {
-        "id": 20,
-        "customerId": 2,
-        "equipmentId": 12,
-        "serialNumber": "5X522H66584"
+        "equipmentName": "PowerChain X",
+        "equipmentType": "Chainsaw",
+        "serialNumber": "456123789",
+        "modelNumber": "PC12K",
+        "manufacturer": "Craftsman",
+        "colour": "Black"
       }
     ];
 }
